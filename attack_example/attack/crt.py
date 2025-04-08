e650fdf7efcc446272695dfe891e0962a31dcfa6
Bui Nguyen <127127056+ngwinis@users.noreply.github.com>
2025-04-08 22:40:37 +0700

    Update crt.py

--- attack_example/attack/crt.py
+++ attack_example/attack/crt.py
@@ -1,27 +1,34 @@
 from Crypto.Util.number import *
-from sympy import root
-
-p1=10086822263673640087
-q1=14616807228050434831
-p2=14347620969203651177
-q2=16538296571908278413
-p3=16618067254672148203
-q3=14799146539459237073
-n1=p1*q1
-n2=p2*q2
-n3=p3*q3
-print(f'{n1=}\n{n2=}\n{n3=}')
-
-e=3
-print(f'{e=}')
+from sympy import *
 
 m = bytes_to_long(b'secret_message')
 
+# Tạo khoá
+p1, q1 = getPrime(512), getPrime(512)
+p2, q2 = getPrime(512), getPrime(512)
+p3, q3 = getPrime(512), getPrime(512)
+e = 3
+n1=p1*q1
+n2=p2*q2
+n3=p3*q3
 c1 = pow(m,e,n1)
 c2 = pow(m,e,n2)
 c3 = pow(m,e,n3)
+
+# Các thành phần được tạo từ server
+print('-'*20, 'Secret component', '-'*20)
+print(f'{m=}\n{p1=}\n{q1=}')
+print(f'{p2=}\n{q2=}')
+print(f'{p3=}\n{q3=}')
+
+# Public
+print('-'*20, 'Public component', '-'*20)
+print(f'{n1=}\n{n2=}\n{n3=}')
+print(f'{e=}')
 print(f'{c1=}\n{c2=}\n{c3=}')
 
+# Attack
+print('-'*25, 'Attack', '-'*25)
 N = n1*n2*n3
 N1 = N//n1
 N2 = N//n2
