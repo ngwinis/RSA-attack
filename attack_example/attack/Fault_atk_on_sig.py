1900a5e2d07fa59ccbb4de6c440beba93d8504db
Bui Nguyen <127127056+ngwinis@users.noreply.github.com>
2025-04-08 23:15:46 +0700

    Update Fault_atk_on_sig.py

--- attack_example/attack/Fault_atk_on_sig.py
+++ attack_example/attack/Fault_atk_on_sig.py
@@ -1,33 +1,34 @@
 from Crypto.Util.number import*
 from functools import reduce
+from math import gcd
 import gmpy2
 
-# with open("flag.txt", "rb") as file:
-#      flag = file.read()
-flag = b'flag{this_is_real_flag}'
-print(flag)
+message = b'Notifications_give_to_staffs'
+# Tạo khoá
 p, q = getPrime(1024), getPrime(1024)
 e = 65537
 n = p*q
-d = inverse(e,(p-1)*(q-1))
-c = pow(bytes_to_long(flag), e, n)
 
-# print(f'{p=}\n{q=}')
-print("n =",n)
-print("e =",e)
-print("c =",c)
+# Các thành phần được tạo từ server
+print('-'*20, 'Secret component', '-'*20)
+print(f'{p=}\n{q=}')
+
+
+# Public
+print('-'*20, 'Public component', '-'*20)
+print(f'{message=}\n{n=}\n{e=}')
 
 def chinese_remainder(rests, modulus):
-        somme = 0
-        prod = reduce(lambda a, b: a * b, modulus)
-        for n_i, a_i in zip(modulus, rests):
-            p = prod // n_i
-            somme += a_i * gmpy2.invert(p, n_i) * p
-        return int(somme % prod)
+    somme = 0
+    prod = reduce(lambda a, b: a * b, modulus)
+    for n_i, a_i in zip(modulus, rests):
+        p = prod // n_i
+        somme += a_i * gmpy2.invert(p, n_i) * p
+    return int(somme % prod)
 
 def sign(m, fault = False):
-    dp = inverse(e,p-1)
-    dq = inverse(e,q-1)
+    dp = int(inverse(e,p-1))
+    dq = int(inverse(e,q-1))
     sp = pow(m, dp, p)
     sq = pow(m, dq, q)
     if fault:
@@ -35,16 +36,13 @@ def sign(m, fault = False):
     s = chinese_remainder([sp, sq], [p, q])
     return s
 
-
-
 while True:
     m = int(input("Nhap m = "))
     s = sign(m, True)
     print(f'{s=}')
 
-    # Test
-    from egcd import egcd
-    found_p, _, _ = egcd(pow(s,e,n)-m, n)
-    print(f'(found_p == p) = {found_p == p}')
+    # Attack
+    print('-'*25, 'Attack', '-'*25)
+    found_p = gcd(pow(s,e,n)-m, n)
+    print(f'found_p = {found_p}')
     break
-
